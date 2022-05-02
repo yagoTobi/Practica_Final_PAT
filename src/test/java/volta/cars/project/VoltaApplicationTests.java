9d817100f6e9110e79da0e2df6ebbfa53e8ef6e1
SaltyYagi123 <201802168@alu.comillas.edu>
2022-05-02 19:06:58 +0200

    first commit

---
+++ src/test/java/volta/cars/project/VoltaApplicationTests.java
@@ -0,0 +1,13 @@
+package volta.cars.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoltaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
