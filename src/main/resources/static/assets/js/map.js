34f05266383bf6da19b1f86cda195c17de552002
Luis Bueno Archaga <luisibien@gmail.com>
2022-05-20 22:26:51 +0200

    master

--- src/main/resources/static/assets/js/map.js
+++ src/main/resources/static/assets/js/map.js
@@ -22,60 +22,75 @@ async function getChargers() {
         data = await request.json();
         console.log(data);
 
-        for (let i = 0; i < data.length; i++) {
+        for (let i = 1; i-1 < data.length; i++) {
 
             async function getCharger() {
-                const { chargerId, chargerType, latitude, longitude, userId } = data[i];
+                const { chargerId, chargerType, latitude, longitude, userId } = data[i-1];
                 const marcador = L.marker([latitude, longitude], { icon: chargerIcon }).addTo(map);
                 map.setView([latitude, longitude], map.getZoom());
                 marcador.setLatLng([latitude, longitude]);
-                marcador.bindPopup('<h3>Tipo de cargador:</h3><h4>' + chargerType + '</h4><div class="row d-flex justify-content-center"><button class="btn btn-dark btn-lg btn-block" id= "reservation_button" type="button">Reservar</button></div>').openPopup();
+                marcador.bindPopup('<h3>Tipo de cargador:</h3><h4>' + chargerType + '</h4><div class="row d-flex justify-content-center"><button class="btn btn-dark btn-lg btn-block" id= "reservation_button' + i + '" type="button">Reservar</button></div>').openPopup();
             }
 
             getCharger();
         }
 
+
+
+        for (let i = 1; i-1 < data.length; i++) {
+            var reservation_button=document.getElementById("reservation_button" + i);
+            console.log(reservation_button);
+            reservation_button.addEventListener("click",
+                (postNewReservation = async () => {
+
+                    const now = new Date();
+                    const reservationHour = now.getHours() + ':' + now.getMinutes();
+                    const reservationDate = now.getDate();
+                    let request2 = await fetch("/api/v1/charger/" + i, {
+                        method: "GET",
+                    }).catch(console.error);
+
+                    if (request2.ok) {
+                        data2 = await request.json();
+                        console.log(data);
+
+                        let request1 = await fetch("/api/v1//hostMembers/" + data2[0].userId, {
+                            method: "GET",
+                        }).catch(console.error);
+
+                        if (request1.ok) {
+                            data1 = await request1.json();
+
+                            let request = await fetch("/api/v1/reservations", {
+                                method: 'POST',
+                                credentials: "same-origin",
+                                headers: {
+                                    "Content-Type": "application/json"
+                                },
+                                body: JSON.stringify({
+                                    userId: data2[0].userId,
+                                    hostId: data1[0].hostId,
+                                    price: data1[0].hourlyrate,
+                                    latitude: data2[0].latitude,
+                                    longitude: data2[0].longitude,
+                                    reservationDate: reservationDate,
+                                    reservationHour: reservationHour
+                                }), dataType: "json"
+                            })
+
+                            console.log(userName);
+
+                            if (request.ok) {
+                                console.log("Success!");
+                                console.log(await request.json());
+                            }
+                        }
+                    }
+                }
+                )
+            )
+        }
     }
 }
 getChargers();
 
-let reservation_button = document.getElementById("reservation_button");
-
-reservation_button.addEventListener("click",
-    (postNewReservation = async () => {
-
-        const now = new Date();
-        const reservationHour = now.getHours() + ':' + now.getMinutes();
-        const reservationDate = now.getDate();
-        let request1 = await fetch("/api/v1//hostMembers/" + userId, {
-            method: "GET",
-        }).catch(console.error);
-
-        if (request1.ok) {
-            data1 = await request1.json();
-
-            let request = await fetch("/api/v1/reservations", {
-                method: 'POST',
-                credentials: "same-origin",
-                headers: {
-                    "Content-Type": "application/json"
-                },
-                body: JSON.stringify({
-                    userId: userId,
-                    hostId: data1[0].hostId,
-                    price: data1[0].hourlyrate,
-                    latitude: latitude,
-                    longitude: longitude,
-                    reservationDate: reservationDate,
-                    reservationHour: reservationHour
-                }), dataType: "json"
-            })
-
-            console.log(userName);
-
-            if (request.ok) {
-                console.log("Success!");
-                console.log(await request.json());
-            }
-        }
-    }))
