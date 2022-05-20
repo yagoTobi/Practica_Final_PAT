e9236f7d048c20a577c7b61145c92a2b7cdadb33
Luis Bueno Archaga <luisibien@gmail.com>
2022-05-20 14:14:46 +0200

    master

--- src/main/resources/static/assets/js/map.js
+++ src/main/resources/static/assets/js/map.js
@@ -1,6 +1,6 @@
 const map = L.map('map').setView([0, 0], 5);
 
-const pcIcon = L.icon({
+const chargerIcon = L.icon({
     iconUrl: 'assets/img/charger.png',
     iconSize: [10, 8],
     iconAnchor: [6, 4]
@@ -18,13 +18,12 @@ if (request.ok) {
     console.log(data);
     for (let i = 0; i < data.length; i++) {
 
-        const marcador = L.marker([0, 0], { icon: pcIcon }).addTo(map);
 
         async function getCharger() {
 
             const latitude = data[i].latitude;
             const longitude = data[i].longitude;
-
+            const marcador = L.marker([latitude, longitude], { icon: chargerIcon }).addTo(map);
             map.setView([latitude, longitude], map.getZoom());
             marcador.setLatLng([latitude, longitude]);
 
