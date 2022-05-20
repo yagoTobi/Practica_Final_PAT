ed90e785623b41db60ca57a3a8443a33ce664430
Luis Bueno Archaga <luisibien@gmail.com>
2022-05-20 13:55:05 +0200

    master

--- src/main/resources/static/assets/js/map.js
+++ src/main/resources/static/assets/js/map.js
@@ -1,34 +1,41 @@
-const map = L.map('map').setView([0,0], 5);
+const map = L.map('map').setView([0, 0], 5);
 
 const pcIcon = L.icon({
     iconUrl: 'assets/img/charger.png',
     iconSize: [10, 8],
     iconAnchor: [6, 4]
-  });
+});
 
-const marcador=L.marker([0,0],{icon:pcIcon}).addTo(map);
+let request = await fetch("/api/v1/chargers", {
+    method: "GET",
+}).catch(console.error);
 
-const attribution ='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
-const tileUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
-const tiles = L.tileLayer(tileUrl, { attribution });
-tiles.addTo(map);
+if (request.ok) {
+    data = await request.json();
+    console.log(data);
+    for (let i = 0; i < data.length; i++) {
 
-const ipFind_url='https://freegeoip.app/json/' 
+        const marcador = L.marker([0, 0], { icon: pcIcon }).addTo(map);
 
-async function getIp () {
-    const response = await fetch(ipFind_url);
-    const data=await response.json();
-    const {ip,country_name,region_name,latitude, longitude}= data;
-    
-    map.setView([latitude, longitude], map.getZoom());
-    marcador.setLatLng([latitude,longitude]);
+        const attribution = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
+        const tileUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+        const tiles = L.tileLayer(tileUrl, { attribution });
+        tiles.addTo(map);
 
-    document.getElementById('ip').textContent=ip;
-    document.getElementById('pais').textContent=country_name;
-    document.getElementById('region').textContent=region_name;
-    document.getElementById('latitud').textContent=latitude.toFixed(1);
-    document.getElementById('longitud').textContent=longitude.toFixed(1);
+        async function getIp() {
+
+            const latitude = data[i].latitude;
+            const longitude = data[i].longitude;
+
+            map.setView([latitude, longitude], map.getZoom());
+            marcador.setLatLng([latitude, longitude]);
+
+        }
+
+    }
+
 }
+
 
 getIp();
 
