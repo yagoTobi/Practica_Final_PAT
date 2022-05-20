ba2101c9ac4a78b41c20b995753d866638375504
Luis Bueno Archaga <luisibien@gmail.com>
2022-05-20 10:14:55 +0200

    master

--- src/main/resources/static/assets/js/login.js
+++ src/main/resources/static/assets/js/login.js
@@ -22,25 +22,6 @@ submit_button.addEventListener("click",
                 document.getElementById("result").innerHTML = "";
 
                 console.log(data);
-
-                let table = document.createElement("table");
-                let row = document.createElement("tr");
-                var cell1 = row.insertCell(0);
-                var cell2 = row.insertCell(1);
-                var cell3 = row.insertCell(2);
-                var cell4 = row.insertCell(3);
-                var cell5 = row.insertCell(4);
-                var cell6 = row.insertCell(5);
-
-                cell1.innerHTML = data.userId;
-                cell2.innerHTML = data.userName;
-                cell3.innerHTML = data.surname;
-                cell4.innerHTML = data.email;
-                cell5.innerHTML = data.password;
-                cell6.innerHTML = data.dob;
-
-                table.appendChild(row);
-                document.getElementById("result").appendChild(table);
             }
 
             if (request1.ok) {
--- src/main/resources/static/assets/js/login_hosts.js
+++ src/main/resources/static/assets/js/login_hosts.js
@@ -23,29 +23,10 @@ submit_button.addEventListener("click",
                 document.getElementById("result").innerHTML = "";
 
                 console.log(data);
-
-                let table = document.createElement("table");
-                let row = document.createElement("tr");
-                var cell1 = row.insertCell(0);
-                var cell2 = row.insertCell(1);
-                var cell3 = row.insertCell(2);
-                var cell4 = row.insertCell(3);
-                var cell5 = row.insertCell(4);
-                var cell6 = row.insertCell(5);
-
-                cell1.innerHTML = data.userId;
-                cell2.innerHTML = data.userName;
-                cell3.innerHTML = data.surname;
-                cell4.innerHTML = data.email;
-                cell5.innerHTML = data.password;
-                cell6.innerHTML = data.dob;
-
-                table.appendChild(row);
-                document.getElementById("result").appendChild(table);
             }
 
             if (request1.ok) {
-                if (table.email == email && table.password == password) {
+                if (data.email == email && data.password == password) {
                     let url = "http://localhost:" + Integer.toString(port) + "/hosts.html";
                     window.location = url;
                 }
