5870284dd3223737917c0a8188a1d7fed0332ebe
Luis Bueno Archaga <luisibien@gmail.com>
2022-05-20 19:38:20 +0200

    master

--- src/main/resources/static/assets/js/map.js
+++ src/main/resources/static/assets/js/map.js
@@ -1,9 +1,9 @@
-const map = L.map('map').setView([0, 0], 2);
+const map = L.map('map').setView([0, 0], 6);
 
 const chargerIcon = L.icon({
     iconUrl: 'assets/img/charger.png',
-    iconSize: [20, 16],
-    iconAnchor: [12, 8]
+    iconSize: [80, 64],
+    iconAnchor: [48, 32]
 });
 
 
@@ -12,26 +12,29 @@ const tileUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
 const tiles = L.tileLayer(tileUrl, { attribution });
 tiles.addTo(map);
 
-let request = await fetch("/api/v1/chargers", {
-    method: "GET",
-}).catch(console.error);
+async function getChargers() {
 
-if (request.ok) {
-    data = await request.json();
-    console.log(data);
+    let request = await fetch("/api/v1/chargers", {
+        method: "GET",
+    }).catch(console.error);
 
-    for (let i = 0; i < data.length; i++) {
+    if (request.ok) {
+        data = await request.json();
+        console.log(data);
 
-        async function getCharger() {
-            const {latitude} = data[i].latitude;
-            const {longitude} = data[i].longitude;
-            const marcador = L.marker([latitude, longitude], { icon: chargerIcon }).addTo(map);
-            map.setView([latitude, longitude], map.getZoom());
-            marcador.setLatLng([latitude, longitude]);
-            marcador.bindPopup('<button class="btn btn-dark btn-lg btn-block" id= "reservation_button" type="button">Reservar</button>').openPopup();
+        for (let i = 0; i < data.length; i++) {
+
+            async function getCharger() {
+                const { chargerId,chargerType,latitude,longitude ,userId} = data[i];
+                const marcador = L.marker([latitude, longitude], { icon: chargerIcon }).addTo(map);
+                map.setView([latitude, longitude], map.getZoom());
+                marcador.setLatLng([latitude, longitude]);
+                marcador.bindPopup('<button class="btn btn-dark btn-lg btn-block" id= "reservation_button" type="button">Reservar</button>').openPopup();
+            }
+
+            getCharger();
         }
 
-        getCharger();
     }
-
 }
+getChargers();
